51a656c4d740e5653674b3a84a60e5cffbc94699
Kacper Rogowski <rogemus@gmail.com>
2021-05-25 18:25:06 +0200

    cleanup styles

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,12 @@
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.svg';
-declare module '*.otf';
 declare module '*.mp3';
 declare module '*.wav';
 
 interface CanvasRenderingContext2D {
-  roundRect: (x: number, y: number, width: number, height: number, radius: number) => void;
+  roundRect: (
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    radius: number
+  ) => void;
 }
